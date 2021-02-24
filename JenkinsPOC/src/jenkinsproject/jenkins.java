ff37f856a8b58241e386a4e1c30d8059f82ff3ff
Pradeep <Pradeep@LAPTOP-82NDSM1Q>
2021-02-24 16:56:37 +0530

    add script has been updated

--- JenkinsPOC/src/jenkinsproject/jenkins.java
+++ JenkinsPOC/src/jenkinsproject/jenkins.java
@@ -3,6 +3,13 @@ package jenkinsproject;
 public class jenkins {
 	
 	public static void main(String[] args) {
+		
+		int a = 10;
+		int b =10;
+		int c = a=b;
+		System.out.println("output of c is : "+ c);
+		
+		
 		System.out.println("hello world");
 	}
 
